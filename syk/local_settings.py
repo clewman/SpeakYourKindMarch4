91ace75b192a064367c715b17581fe4cc864c71c
clewman <cheryllewman@gmail.com>
2019-03-04 12:49:39 -0800

    adding local settings

---
+++ syk/local_settings.py
@@ -0,0 +1,14 @@
+STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = '6xg5%7yddb5v%%-n7l8@$_27r53bfy0-pbbt2c9js3w#wac0@='
--- syk/settings.py
+++ syk/settings.py
@@ -14,18 +14,16 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+from syk.local_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6xg5%7yddb5v%%-n7l8@$_27r53bfy0-pbbt2c9js3w#wac0@='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,12 +73,7 @@ WSGI_APPLICATION = 'syk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
@@ -119,7 +112,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 LOGIN_URL = '/users/register/'
 AUTH_USER_MODEL='game.Person'
-
