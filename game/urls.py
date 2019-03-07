6cbe40ed86c8dc2c3a9db61555af5642750100be
Cheryl L <cheryllewman@gmail.com>
2019-03-06 21:49:55 -0800

    Add files via upload
    added url for to see game scores

--- game/urls.py
+++ game/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = 'game'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('scores', views.player_score, name='player_score'),
+
     path('game/', views.new_game, name='game'),
     path('game_play/<int:pk>/', views.game_play, name='game_play'),
     path('get_tiles/<int:pk>/', views.get_tiles, name='get_tiles'),
